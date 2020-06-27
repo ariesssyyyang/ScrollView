5be965ae270819c2a6f9957867ff96a3a3f39d83
Aries Yang <ariesssyyyang@gmail.com>
2020-06-27 22:56:54 +0800

    Initial commit

---
+++ LingualNumbers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LingualNumbers
+//
+//  Created by Aries Yang on 2020/6/27.
+//  Copyright © 2020 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
