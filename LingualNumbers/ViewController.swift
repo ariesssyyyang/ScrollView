884644b703fb20a0498392e5a37e7c63826083f4
Aries Yang <ariesssyyyang@gmail.com>
2020-06-28 23:05:36 +0800

    Add views

--- LingualNumbers/ViewController.swift
+++ LingualNumbers/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI
+    let tabsView: UIScrollView = UIScrollView()
+    let numbersView: UIScrollView = UIScrollView()
+    let indicator: UIView = UIView()
+
+    // MARK: - Constant
+    var TAB_WIDTH: CGFloat { view.bounds.width / 2 }
+    let TAB_HEIGHT: CGFloat = 50
+    var PADDING_WIDTH: CGFloat { view.bounds.width / 4 }
+
+    // MARK: - View Model
     let viewModel = ViewModel([
         NumberList(.english),
         NumberList(.chinese),
@@ -21,7 +32,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        setupView()
     }
 }
 
+private extension ViewController {
+    func setupView() {
+        view.backgroundColor = .yellow
+
+        view.addSubview(tabsView)
+        tabsView.frame = CGRect(
+            x: PADDING_WIDTH, y: UIApplication.shared.statusBarFrame.height,
+            width: TAB_WIDTH, height: TAB_HEIGHT
+        )
+        tabsView.backgroundColor = .green
+
+        view.addSubview(indicator)
+        indicator.frame = CGRect(
+            x: PADDING_WIDTH, y: tabsView.frame.maxY - 2,
+            width: tabsView.frame.width, height: 2
+        )
+        indicator.backgroundColor = .red
+
+        view.addSubview(numbersView)
+        numbersView.frame = CGRect(
+            x: 0, y: tabsView.frame.maxY,
+            width: view.bounds.width, height: view.bounds.height - tabsView.frame.maxY
+        )
+        numbersView.backgroundColor = .orange
+    }
+}
